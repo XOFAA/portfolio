8c56e2a3390845fd42f8ba3b2ea7f5d66c660d45
xofaa <giovanne.oliv@gmail.com>
2023-04-25 13:32:03 -0300

    alterando hover projetos

--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -25,75 +25,91 @@ export const Projetos = () => {
             </Container>
             <Swiper
                 // install Swiper modules
-                modules={[Pagination, Scrollbar, A11y,Navigation]}
+                modules={[Pagination, Scrollbar, A11y, Navigation]}
                 spaceBetween={50}
                 breakpoints={{
-                    600:{
-                        slidesPerView:1
+                    600: {
+                        slidesPerView: 1
                     },
-                    768:{
-                        slidesPerView:2
+                    768: {
+                        slidesPerView: 2
                     },
-                    1024:{
-                        slidesPerView:3
+                    1024: {
+                        slidesPerView: 3
                     }
 
-                    
-                    
+
+
                 }}
-             
-              
+
+
                 navigation
             >
                 <SwiperSlide>
-                    <Box sx={{ borderRadius: 2, height: 350, display: 'flex', flexDirection: 'column', justifyContent: "end", border: 2,  backgroundImage: 'url(/images/projeto1.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    <Box sx={{ display: 'flex', padding: 2,  height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
-                    <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 1</Typography>
-                     </Box>
+                    <Box sx={{
+                        borderRadius: 2,
+                        height: 350,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: "end",
+                        border: 2,
+                       
+                        backgroundImage: 'url(/images/projeto1.png)',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        transition: 'border-color 1.5s ease',
+                        '&:hover': {
+                            borderColor: 'green',
+                        },
+                    }}>
+                        <Box sx={{ display: 'flex', padding: 2, height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
+                            <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 1</Typography>
+                        </Box>
+                    </Box>
+
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Box sx={{ borderRadius: 2, height: 350, display: 'flex', flexDirection: 'column', justifyContent: "end", border: 2, backgroundImage: 'url(/images/projeto1.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                        <Box sx={{ display: 'flex', padding: 2, height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
+                            <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 2</Typography>
+                        </Box>
                     </Box>
                 </SwiperSlide>
 
                 <SwiperSlide>
                     <Box sx={{ borderRadius: 2, height: 350, display: 'flex', flexDirection: 'column', justifyContent: "end", border: 2, backgroundImage: 'url(/images/projeto1.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    <Box sx={{ display: 'flex', padding: 2,  height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
-                    <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 2</Typography>
-                     </Box>
-                    </Box>
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <Box sx={{ borderRadius: 2, height: 350, display: 'flex', flexDirection: 'column', justifyContent: "end", border: 2,  backgroundImage: 'url(/images/projeto1.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    <Box sx={{ display: 'flex', padding: 2,  height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
-                    <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 3</Typography>
-                     </Box>
+                        <Box sx={{ display: 'flex', padding: 2, height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
+                            <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 3</Typography>
+                        </Box>
                     </Box>
                 </SwiperSlide>
 
                 <SwiperSlide>
                     <Box sx={{ borderRadius: 2, height: 350, display: 'flex', flexDirection: 'column', justifyContent: "end", border: 2, backgroundImage: 'url(/images/projeto1.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    <Box sx={{ display: 'flex', padding: 2,  height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
-                    <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 4</Typography>
-                     </Box>
+                        <Box sx={{ display: 'flex', padding: 2, height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
+                            <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 4</Typography>
+                        </Box>
                     </Box>
                 </SwiperSlide>
 
                 <SwiperSlide>
                     <Box sx={{ borderRadius: 2, height: 350, display: 'flex', flexDirection: 'column', justifyContent: "end", border: 2, minWidth: 250, backgroundImage: 'url(/images/projeto1.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    <Box sx={{ display: 'flex', padding: 2,  height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
-                    <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 5</Typography>
-                     </Box>
+                        <Box sx={{ display: 'flex', padding: 2, height: 70, bgcolor: 'rgba(76,76,76,0.4)' }}>
+                            <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Projeto 5</Typography>
+                        </Box>
                     </Box>
                 </SwiperSlide>
 
-         
 
-              
 
-         
 
-                
-               
-                
+
+
+
+
+
+
 
             </Swiper>
         </Box>
